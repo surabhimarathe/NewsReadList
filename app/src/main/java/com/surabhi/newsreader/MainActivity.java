a3ff42d716185446e3565fa655c1790a9c7b41dd
SurabhiMarathe <srm6226@rit.edu>
2019-01-28 23:19:48 -0500

    get top stories from HackerNews

---
+++ app/src/main/java/com/surabhi/newsreader/MainActivity.java
@@ -0,0 +1,71 @@
+package com.surabhi.newsreader;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    ArrayList<String> titles = new ArrayList<>();
+    ArrayAdapter arrayAdapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, titles);
+
+        DownLoadTask task =  new DownLoadTask();
+        try {
+
+            task.execute("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty");
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public class DownLoadTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... strings) {
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnection = null;
+            try {
+                url = new URL(strings[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream in = urlConnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(in);
+                int data = reader.read();
+                while (data != -1) {
+                    char currentChar = (char) data;
+                    result += currentChar;
+                    data = reader.read();
+                }
+
+                Log.i("URL Content", result);
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+    }
+
+}
